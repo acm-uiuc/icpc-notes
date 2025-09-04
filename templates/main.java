882071180e48d0d4230e1cfff430e35d8b7ee130
3nya <enyac1207@gmail.com>
2025-09-04 17:12:24 -0500

    add templates

---
+++ templates/main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        // input
+        int n = scan.nextInt();
+        
+        // output
+        System.out.println(n);
+
+    }
+}
